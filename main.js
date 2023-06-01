b6ec3ac3b10311426503c17039195da44652a685
Jurjen de Vries <jurjen@jurjendevries.com>
2023-06-01 16:16:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 window.onload = () => {
   loadSettings();
   requestNotificationPermissions();
+  registerServiceWorker();
 };
+
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  }
+}
 
 function loadSettings() {
   document.getElementById('api-key').value = localStorage.getItem('openai_api_key') || '';
