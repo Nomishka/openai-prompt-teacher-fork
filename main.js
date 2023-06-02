afe1501a79da289edd226237dc120065f1999ad4
Jurjen de Vries <jurjen@jurjendevries.com>
2023-06-02 17:27:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,77 +1,23 @@
-window.onload = () => {
-  loadSettings();
-  requestNotificationPermissions();
-  registerServiceWorker();
-};
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
 
-function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch(function(error) {
-      console.log('Service Worker registration failed:', error);
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
     });
   }
 }
 
-function loadSettings() {
-  document.getElementById('api-key').value = localStorage.getItem('openai_api_key') || '';
-  document.getElementById('notification-time').value = localStorage.getItem('notification_time') || '';
-}
+document.getElementById('show').addEventListener('click', () => {
+  displayNotification();
+});
 
-function saveSettings() {
-  localStorage.setItem('openai_api_key', document.getElementById('api-key').value);
-  localStorage.setItem('notification_time', document.getElementById('notification-time').value);
-
-  // Set a new notification
-  setDailyNotification(document.getElementById('notification-time').value);
-}
-
-function requestNotificationPermissions() {
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  } else if (Notification.permission === 'granted') {
-    console.log('Notification permission granted.');
-  } else {
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      } else {
-        console.log('Unable to get permission to notify.');
-      }
-    });
-  }
-}
-
-function setDailyNotification(time) {
-  const [hour, minute] = time.split(':');
-  const notificationDate = new Date();
-  notificationDate.setHours(hour);
-  notificationDate.setMinutes(minute);
-
-  if (notificationDate < new Date()) {
-    notificationDate.setDate(notificationDate.getDate() + 1);
-  }
-
-  const delay = notificationDate.getTime() - new Date().getTime();
-  setTimeout(() => sendNotification(), delay);
-}
-
-function sendNotification() {
-  if (Notification.permission === 'granted') {
-    new Notification('Time to practice your prompting skills!');
-  }
-}
-
-function practicePrompting() {
-  const prompt = 'Today\'s prompt: Write a story about a cat named Tom.';
-  const userResponse = window.prompt(prompt);
-
-  // assuming you have a function that uses the OpenAI API to grade the response
-  // const feedback = gradeResponse(getApiKey(), userResponse);
-  
-  // For the purpose of this example, let's just mimic the feedback.
-  const feedback = `Your response was: ${userResponse}. Keep practicing to improve!`;
-  
-  alert(feedback);
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/openai-prompt-teacher/sw.js');
+  });
 }
